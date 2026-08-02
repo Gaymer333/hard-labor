d3535d99a47eb7653f086d195c71d9016cc40cac
Peter Andresen <pa@onomondo.com>
2026-08-02 21:28:42 +0900

    Visual update

--- src/containers/Games/Mine/index.tsx
+++ src/containers/Games/Mine/index.tsx
@@ -6,6 +6,7 @@ import _2hit from "../../../assets/images/games/mine/2hit.jpg";
 import _2swing from "../../../assets/images/games/mine/2swing.jpg";
 import _3hit from "../../../assets/images/games/mine/3hit.jpg";
 import _3swing from "../../../assets/images/games/mine/3swing.jpg";
+import _copperUrl from "../../../assets/images/resources/cooper.png";
 
 const hitImages = [_1hit, _2hit, _3hit];
 const swingImages = [_1swing, _2swing, _3swing];
@@ -24,34 +25,68 @@ const SideWrapper = styled(Wrapper)`
   width: 50%;
 `;
 
-const BAR_WIDTH = 300;
-const BAR_SIDE_SAFE_MARGIN = 20;
-const TARGET_DEFAULT_LENGTH = 50;
+const AbsoluteWrapper = styled.div`
+  position: absolute;
+  bottom: 50px;
+  padding: 20px;
+  width: 50%;
+  background-color: rgba(5, 70, 0, 0.8);
+  border-radius: 10px;
+  border: 3px solid #002412;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BAR_WIDTH = 800;
+const BAR_HEIGHT = 40;
+const BAR_SIDE_SAFE_MARGIN = 10;
+const TARGET_SIDE_SAFE_MARGIN = 30;
+const TARGET_DEFAULT_LENGTH = 100;
+
+const ResourceDisplay = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  margin-bottom: 20px;
+  font-size: 24px;
+  color: #000000;
+  background-color: rgb(255, 222, 124);
+  padding: 5px 10px;
+  border-radius: 10px;
+  border: 3px solid #002412;
+`;
 
 const BarBackground = styled.div`
   width: ${BAR_WIDTH}px;
-  height: 20px;
-  background-color: #00260a;
+  height: ${BAR_HEIGHT}px;
+  background-color: #000000;
   border-radius: 10px;
-`
+  border: 2px solid #002412;
+`;
 
 const Target = styled.div<{ start: number; length: number }>`
   position: relative;
   top: 0;
   left: ${props => props.start}px;
   width: ${props => props.length}px;
-  height: 20px;
-  background-color: #20c720;
+  height: ${BAR_HEIGHT - 4}px;
+  background-color: #c77100;
+  border-radius: 10px;
+  border: 2px solid #ffb84d;
 `;
 
 const AimLine = styled.div.attrs<{ position: number }>(props => ({
   style: { left: `${props.position}px` },
 }))`
   position: relative;
-  top: -20px;
-  width: 2px;
-  height: 20px;
-  background-color: red;
+  top: -${BAR_HEIGHT}px;
+  width: 10px;
+  height: ${BAR_HEIGHT - 4}px;
+  background-color: #989898;
+  border-radius: 5px;
+  border: 2px solid #5c5c5c;
 `;
 
 interface TargetDetails {
@@ -70,7 +105,7 @@ function Mine() {
   const swing = swingImages[Math.min(Math.floor(score / 10), swingImages.length - 1)];
 
   function generateTargetDetails() {
-    const start = Math.floor(Math.random() * (BAR_WIDTH - TARGET_DEFAULT_LENGTH - 2 * BAR_SIDE_SAFE_MARGIN)) + BAR_SIDE_SAFE_MARGIN;
+    const start = Math.floor(Math.random() * (BAR_WIDTH - TARGET_DEFAULT_LENGTH - 2 * TARGET_SIDE_SAFE_MARGIN)) + TARGET_SIDE_SAFE_MARGIN;
     const length = TARGET_DEFAULT_LENGTH;
     setTargetDetails({ start, length });
   }
@@ -82,7 +117,7 @@ function Mine() {
   useEffect(() => {
     const aimMoveingInterval = setInterval(() => {
       setAimPosition(prev => {
-        const newPosition = prev + 1;
+        const newPosition = prev + 2.2;
         if (newPosition > BAR_WIDTH - BAR_SIDE_SAFE_MARGIN) {
           return BAR_SIDE_SAFE_MARGIN; // Reset to the left side if it goes beyond the bar width
         }
@@ -131,13 +166,15 @@ function Mine() {
 
   return (
     <Wrapper>
-      <SideWrapper>
-        <div>Score: {score}</div>
+      <AbsoluteWrapper>
+        <ResourceDisplay>
+          <img src={_copperUrl} alt="Copper" style={{ height: "32px", width: "32px" }} />: {score}
+        </ResourceDisplay>
         <BarBackground>
           {targetDetails && <Target start={targetDetails.start} length={targetDetails.length} />}
           <AimLine position={aimPosition} />
         </BarBackground>
-      </SideWrapper>
+      </AbsoluteWrapper>
       <SideWrapper>
         {showHitImage ? (
           <img src={hit} alt="Hit" style={{ height: "100%", width: "auto" }} />
